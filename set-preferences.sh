cbd20357a46d01b3065f53846257f7cc5ff3ee95
Jonathan Dahan <jonathan@jedahan.com>
2012-10-16 17:32:04 -0400

    only set osx preferences

--- set-preferences.sh
+++ set-preferences.sh
@@ -1,25 +1,15 @@
 #!/bin/env bash
 
-# turn off dashboard - turn back on w/ boolean NO
-defaults write com.apple.dashboard mcx-disabled -boolean YES 
+## User Interface
 
-### make the dock as unobtrusive as possible
-defaults write com.apple.dock tilesize -int 1
-defaults write com.apple.dock pinning -string start
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# turn off creation of .DS_Store on network drives
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+# Force UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
 
-# tcp segment size to something more 'modern'
-sudo sysctl -w net.inet.tcp.mssdflt=1460
-
-# modify boot time images
-sudo cp BootLogo.png /System/Library/CoreServices/
-sudo cp DefaultDesktop /System/Library/CoreServices/
-
-# Show remaining battery time; hide percentage
-defaults write com.apple.menuextra.battery ShowPercent -string "NO"
-defaults write com.apple.menuextra.battery ShowTime -string "YES"
+# Disable the dashboard
+defaults write com.apple.dashboard mcx-disabled -boolean YES
 
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
@@ -27,14 +17,14 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 # Disable opening and closing window animations
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
-# Increase window resize speed for Cocoa applications
-defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+
+## Finder
 
 # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
 
 # Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons
 defaults write com.apple.finder QuitMenuItem -bool true
@@ -42,14 +32,16 @@ defaults write com.apple.finder QuitMenuItem -bool true
 # Disable window animations and Get Info animations in Finder
 defaults write com.apple.finder DisableAllAnimations -bool true
 
-# Show all filename extensions in Finder
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
 # Allow text selection in Quick Look
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
-# Display full POSIX path as Finder window title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# Open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Disable .DS_Store file creation on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
@@ -60,40 +52,20 @@ defaults write com.apple.finder WarnOnEmptyTrash -bool false
 # Empty Trash securely by default
 defaults write com.apple.finder EmptyTrashSecurely -bool true
 
+# Disable window animations and Get Info animations in Finder
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+
+## Customization
+
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
-# Don’t animate opening applications from the Dock
-defaults write com.apple.dock launchanim -bool false
-
-# Remove the auto-hiding Dock delay
-defaults write com.apple.Dock autohide-delay -float 0
-# Remove the animation when hiding/showing the Dock
-defaults write com.apple.dock autohide-time-modifier -float 0
-
-# Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool true
-
-# Make Dock icons of hidden applications translucent
-defaults write com.apple.dock showhidden -bool true
-
-# Enable Safari’s debug menu
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-# Disable the Ping sidebar in iTunes
-defaults write com.apple.iTunes disablePingSidebar -bool true
-
-# Disable all the other Ping stuff in iTunes
-defaults write com.apple.iTunes disablePing -bool true
-
-# Set my preferred desktop background
-sudo cp Solarized-Darker.png "/Library/Desktop Pictures/Solid Colors/"
-defaults write com.apple.desktop Background '{default = {ImageFilePath = "/Library/Desktop Pictures/Solid Colors/Solarized-Darker.png"; };}'
-
-# Show standard unix directories in finder
-sudo chflags nohidden /bin /etc /usr
+## Cleanup
 
 # Kill affected applications
 for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" > /dev/null 2>&1; done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
-

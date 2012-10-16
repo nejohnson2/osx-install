ba2515a775f40188c9b68dc12c1b4370f994bc36
Jonathan Dahan <jonathan@jedahan.com>
2012-10-16 17:33:09 -0400

    use my applications

--- install-applications.sh
+++ install-applications.sh
@@ -6,48 +6,18 @@ brew tap phinze/homebrew-cask
 brew tap homebrew/dupes
 brew install brew-cask
 
-brew cask install google-chrome 
-brew cask install one-password
-brew cask install adium
-brew cask install alfred
-brew cask install caffeine
-brew cask install dropbox
-brew cask install echofon
-brew cask install firefox
-brew cask install flux
-brew cask install iterm2
-brew cask install sparrow
-brew cask install the-unarchiver
-brew cask install sublime-text
+for app in alfred dropbox google-chrome little-snitch the-unarchiver scrup steam sublime-text transmission
+	brew cask install $app
+done
 
-brew install perl
-brew install python
-brew install ruby
+for language in python ruby
+	brew cask install $language
+done
 
-brew install bazaar
-brew install git
-brew install mercurial
-brew install subversion
+for cli in git ack colordiff node rlwrap zsh hub
+	brew install $cli
+done
 
-brew install ack
-brew install colordiff
-brew install curl
-brew install emacs
-brew install findbugs
-brew install ncftp
-brew install openssl
-brew install p7zip
-brew install pdfgrep
-brew install reattach-to-user-namespace
-brew install rlwrap
-brew install ssh-copy-id
-brew install tmux
-brew install wget
-brew install zsh
-brew install hub
+pip install httpie
 
-# install vim with ruby support for command-t
-brew install https://raw.github.com/Homebrew/homebrew-dupes/master/vim.rb
-# install macvim with ruby support for command-t
-brew install macvim
-
+gem install homesick

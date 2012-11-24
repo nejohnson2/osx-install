536436f2b4299088461fa98a82c38f8cea819dbb
Jonathan Dahan <jonathan@jedahan.com>
2012-11-24 00:19:10 -0500

    install inconsolata font

--- customize-dotfiles.sh
+++ customize-dotfiles.sh
@@ -7,3 +7,8 @@ homesick clone jedahan/dotfiles && homesick symlink jedahan/dotfiles
 # Make zsh default shell
 chsh -s /bin/zsh
 
+## Font
+
+# Install Inconsolata
+curl http://levien.com/type/myfonts/Inconsolata.otf -o ${HOME}/Library/Fonts/Inconsolata.otf
+

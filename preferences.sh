3c720a0221909dc184629146fdf2816758d663be
Jonathan Dahan <jonathan@jedahan.com>
2013-12-17 18:35:49 -0500

    set some hot corners for screensaver and notifications

--- preferences.sh
+++ preferences.sh
@@ -42,6 +42,31 @@ defaults write ~/Library/Preferences/ByHost/com.apple.systemuiserver.* dontAutoL
 # Set clock format
 defaults write com.apple.menuextra.clock DateFormat -string "h:mm"
 
+# Hot corners
+## Possible values:
+##  0: no-op
+##  2: Mission Control
+##  3: Show application windows
+##  4: Desktop
+##  5: Start screen saver
+##  6: Disable screen saver
+##  7: Dashboard
+## 10: Put display to sleep
+## 11: Launchpad
+## 12: Notification Center
+
+# Top left screen corner → Nothing
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tl-modifier -int 0
+
+# Top right screen corner → Notification Center
+defaults write com.apple.dock wvous-tr-corner -int 12
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
 ## Finder
 
 # Finder: Open home folder in new windows

135552c8f29b4f63095eba1a5ba22bb69e4b7dce
Jonathan Dahan <jonathan@jedahan.com>
2014-03-03 12:06:54 -0500

    use ${HOME} instead of ~/

--- sublime.zsh
+++ sublime.zsh
@@ -1,4 +1,4 @@
-$sublime_support_dir='~/Library/Application Support/Sublime Text 3/'
+sublime_support_dir='${HOME}/Library/Application Support/Sublime Text 3/'
 pushd ~/.sublime
 for f in **/*(^/); do
     echo ln -fs ${f} ${(q)sublime_support_dir}/${(q)f}

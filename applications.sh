87a032abe1e9d53623bcbd9edfa6e9f2b1ed6141
Jonathan Dahan <jonathan@jedahan.com>
2014-04-27 09:34:26 -0400

    install beta versions of firefox, iterm2

--- applications.sh
+++ applications.sh
@@ -5,23 +5,25 @@ sudo softwareupdate --install --all
 xcode-select --install
 sudo xcode-select --switch /Library/Developer/CommandLineTools/
 sudo xcodebuild -license
-# Install package managers homebrew, homebrew cask, and curdling (for python)
 
+# Install python package manager and applications
 easy_install curdling
 pys = 'beets httpie'
 brew -v || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 for py in $pys; do curd install $py; done
 
+# Install homebrew package manager and applications
 brew -v && {
   brew tap phinze/homebrew-cask
   brew install brew-cask
+  brew tap caskroom/versions
 
   # Install applications
   brew install --disable-etcdir zsh # because osx is misconfigured
   brew tap gerhard/homebrew-deliver # for deliver
   brew tap homebrew/dupes # for ruby
 
-  apps='alfred arduino cheatsheet chromium daisydisk dogecoin dropbox firefox iterm2 skype transmission virtualbox wireshark xquartz'
+  apps='alfred arduino cheatsheet chromium daisydisk dogecoin dropbox firefox-beta iterm2-beta skype transmission virtualbox wireshark xquartz'
   languages='python ruby'
   clis='android-sdk deliver git hub imagemagick mongodb nmap node redis the_silver_searcher trash watch'
   npm_clis='coffee-script-redux'

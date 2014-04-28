2aead6f372a112a408a370cb97a22b96d017849e
Jonathan Dahan <jonathan@jedahan.com>
2014-04-27 20:10:02 -0400

    use a Brewfile and Caskfile for installing homebrew-managed packages

---
+++ Brewfile
@@ -0,0 +1,24 @@
+update
+upgrade
+
+tap gerhard/homebrew-deliver
+tap homebrew/dupes # for ruby
+
+# languages
+install python
+install ruby
+
+# clis
+install android-sdk
+install deliver
+install git
+install hub
+install imagemagick
+install mongodb
+install nmap
+install node
+install redis
+install the_silver_searcher
+install trash
+install watch
+install --disable-etcdir zsh # because osx is misconfigured
--- applications.sh
+++ applications.sh
@@ -6,34 +6,23 @@ xcode-select --install
 sudo xcode-select --switch /Library/Developer/CommandLineTools/
 sudo xcodebuild -license
 
+# Install homebrew package manager and applications
+brew -v || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew -v && {
+  brew bundle Brewfile
+  brew bundle Caskfile
+  qlmanage -r
+}
+
 # Install python package manager and applications
 easy_install curdling
-pys = 'beets httpie'
-brew -v || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-for py in $pys; do curd install $py; done
+curd install beets
+curd install httpie
 
-# Install homebrew package manager and applications
-brew -v && {
-  brew tap phinze/homebrew-cask
-  brew install brew-cask
-  brew tap caskroom/versions
+# Install coffee-script
+npm install -g coffee-script-redux
 
-  # Install applications
-  brew install --disable-etcdir zsh # because osx is misconfigured
-  brew tap gerhard/homebrew-deliver # for deliver
-  brew tap homebrew/dupes # for ruby
-
-  apps='alfred arduino atom cheatsheet chromium daisydisk dogecoin dropbox firefox-beta iterm2-beta skype transmission virtualbox wireshark xquartz'
-  languages='python ruby'
-  clis='android-sdk deliver git hub imagemagick mongodb nmap node redis the_silver_searcher trash watch'
-  npm_clis='coffee-script-redux'
-  rubygems='foreman homesick lolcat lolcommits'
-  quicklook_plugins='qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package'
-
-  for app in $apps; do brew cask install $app; done
-  for language in $languages; do brew install $language; done
-  for cli in $clis; do brew install $cli; done
-  for npm_cli in $npm_clis; do npm install -g $npm_cli; done
-  sudo gem install $rubygems
-  for quicklook in $quicklook_plugins; do brew cask install $plugin; done && qlmanage -r
-}
+# Install some more cli utilities
+export GEM_HOME="${HOME}/.gems"
+export GEM_PATH="${HOME}/.gems"
+gem install foreman homesick lolcat lolcommits

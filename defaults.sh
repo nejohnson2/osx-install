65f746bddb73ab213d0cbf57a2f059aab9921ae2
Jonathan Dahan <jonathan@jedahan.com>
2014-05-02 09:49:48 -0400

    better spacing / readability

--- defaults.sh
+++ defaults.sh
@@ -1,14 +1,18 @@
 # Transmission: Trash original torrent files
 defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+
 # Transmission: Hide the donate and legal messages
 defaults write org.m0k.transmission WarningDonate -bool false
 defaults write org.m0k.transmission WarningLegal -bool false
+
 # Transmission: Setup Blocklist
 defaults write org.m0k.transmission BlocklistURL -string "http://list.iblocklist.com/?list=bt_level1&fileformat=p2p&archiveformat=gz"
 defaults write org.m0k.transmission BlocklistAutoUpdate -bool true
 defaults write org.m0k.transmission BlocklistNew -bool true
+
 # Transmission: Require Encryption
 defaults write org.m0k.transmission EncryptionRequire -bool true
+
 # Transmission: Minimal UI
 defaults write org.m0k.transmission SmallView -bool true
 defaults write org.m0k.transmission InfoVisible -bool false
